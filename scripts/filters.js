90f1b168bc8e984be739cc241fdee62554f87031
t3mm1k <t3mm1k@gmail.com>
2025-03-18 22:44:49 +0700

    Filters animation

---
+++ scripts/filters.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const filterButton = document.querySelector('.filter-button');
+    const filtersContainer = document.querySelector('.filters-container');
+    const footerButtons = document.querySelector('.footer-buttons');
+
+
+    filterButton.addEventListener('click', function() {
+        filtersContainer.classList.toggle('active');
+        filterButton.classList.toggle('active');
+
+        if (filtersContainer.classList.contains('active')) {
+             filtersContainer.style.display = 'flex';
+             const maxHeight = filtersContainer.scrollHeight + 'px';
+             filtersContainer.style.maxHeight = '0px';
+             filtersContainer.offsetHeight;
+             filtersContainer.style.maxHeight = maxHeight;
+             footerButtons.classList.remove('top-radius');
+
+        } else {
+            filtersContainer.style.maxHeight = '0px';
+            setTimeout(() => {
+               filtersContainer.style.display = 'none'; 
+              }, 500);
+              setTimeout(() => {
+                footerButtons.classList.add('top-radius');
+                }, 100);
+            
+        }
+    });
+
+    //  // Добавляем обработчики для кнопок-переключателей
+    // const toggleButtons = document.querySelectorAll('.toggle-button');
+    // toggleButtons.forEach(button => {
+    //     button.addEventListener('click', function() {
+    //         // Снимаем класс active со всех кнопок в той же секции
+    //         const parentSection = button.closest('.filter-section');
+    //         const parentMarketplaces = button.closest('.marketplaces');
+        
+    //         if (parentSection && !parentMarketplaces) {
+    //             parentSection.querySelectorAll('.toggle-button').forEach(btn => {
+    //                 btn.classList.remove('active');
+    //                 const circle = btn.querySelector('.circle');
+    //                 if(circle){
+    //                      circle.classList.remove('active');
+    //                 }
+    //             });
+    //         }
+    //         button.classList.add('active');
+    //         const circle = button.querySelector('.circle');
+    //             if(circle){
+    //                 circle.classList.add('active');
+    //         }
+    //     });
+    // });
+});
